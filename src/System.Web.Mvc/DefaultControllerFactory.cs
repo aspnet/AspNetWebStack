34827991835a1d14b9e225495d4db4577930d99d
Doug Bunting <dougbu@microsoft.com>
2017-10-08 14:00:04 -0700

    Add additional `requestContext` and `requestContext.RouteData` null checks - #6

--- src/System.Web.Mvc/DefaultControllerFactory.cs
+++ src/System.Web.Mvc/DefaultControllerFactory.cs
@@ -141,7 +141,8 @@ namespace System.Web.Mvc
                 throw new ArgumentNullException("requestContext");
             }
 
-            if (String.IsNullOrEmpty(controllerName) && !requestContext.RouteData.HasDirectRouteMatch())
+            if (String.IsNullOrEmpty(controllerName) &&
+                (requestContext.RouteData == null || !requestContext.RouteData.HasDirectRouteMatch()))
             {
                 throw new ArgumentException(MvcResources.Common_NullOrEmpty, "controllerName");
             }
@@ -153,6 +154,10 @@ namespace System.Web.Mvc
 
         protected internal virtual IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
+            if (requestContext == null)
+            {
+                throw new ArgumentNullException("requestContext");
+            }
             if (controllerType == null)
             {
                 throw new HttpException(404,
@@ -170,6 +175,7 @@ namespace System.Web.Mvc
                         controllerType),
                     "controllerType");
             }
+
             return ControllerActivator.Create(requestContext, controllerType);
         }
 
@@ -214,7 +220,7 @@ namespace System.Web.Mvc
             // first search in the current route's namespace collection
             object routeNamespacesObj;
             Type match;
-            if (routeData.DataTokens.TryGetValue(RouteDataTokenKeys.Namespaces, out routeNamespacesObj))
+            if (routeData != null && routeData.DataTokens.TryGetValue(RouteDataTokenKeys.Namespaces, out routeNamespacesObj))
             {
                 IEnumerable<string> routeNamespaces = routeNamespacesObj as IEnumerable<string>;
                 if (routeNamespaces != null && routeNamespaces.Any())
@@ -232,10 +238,11 @@ namespace System.Web.Mvc
             }
 
             // then search in the application's default namespace collection
+            RouteBase route = routeData == null ? null : routeData.Route;
             if (ControllerBuilder.DefaultNamespaces.Count > 0)
             {
                 HashSet<string> namespaceDefaults = new HashSet<string>(ControllerBuilder.DefaultNamespaces, StringComparer.OrdinalIgnoreCase);
-                match = GetControllerTypeWithinNamespaces(routeData.Route, controllerName, namespaceDefaults);
+                match = GetControllerTypeWithinNamespaces(route, controllerName, namespaceDefaults);
                 if (match != null)
                 {
                     return match;
@@ -243,7 +250,7 @@ namespace System.Web.Mvc
             }
 
             // if all else fails, search every namespace
-            return GetControllerTypeWithinNamespaces(routeData.Route, controllerName, null /* namespaces */);
+            return GetControllerTypeWithinNamespaces(route, controllerName, null /* namespaces */);
         }
 
         private static Type GetControllerTypeFromDirectRoute(RouteData routeData)
