1737a1da29496b8a4607b9b8588d033a01abd7e9
Doug Bunting <dougbu@microsoft.com>
2017-10-09 10:43:33 -0700

    Move to MSBuild v15.0 - change `CultureUtilTest` to use `Thread.CurrentThread`   - when hosted in latest MSBuild, changing `thread.CurrentThread` changes `Thread.CurrentThread` instead - find current Microsoft.Build.Tasks assembly in WebStack.tasks.targets, not just the WebApiHelpPage project - add `$(TestInParallel)`   - allows user to e.g. build in parallel but test assemblies serially   - default remains doing everything in parallel - change build.cmd to set %ErrorLevel% when exiting - add empty top-level Directory.Build.* files

--- test/System.Web.WebPages.Test/Utils/CultureUtilTest.cs
+++ test/System.Web.WebPages.Test/Utils/CultureUtilTest.cs
@@ -13,11 +13,12 @@ namespace System.Web.WebPages.Test
     public class CultureUtilTest
     {
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureWithNoUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(null);
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentCulture;
 
             // Act
@@ -28,11 +29,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureWithNoUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(null);
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentUICulture;
 
             // Act
@@ -43,11 +45,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureWithEmptyUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(Enumerable.Empty<string>());
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentCulture;
 
             // Act
@@ -58,11 +61,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureWithEmptyUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(Enumerable.Empty<string>());
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentUICulture;
 
             // Act
@@ -73,11 +77,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureWithBlankUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { " " });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentCulture;
 
             // Act
@@ -88,11 +93,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureWithBlankUserLanguagesDoesNothing()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { " " });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentUICulture;
 
             // Act
@@ -103,12 +109,13 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureWithInvalidLanguageDoesNothing()
         {
             // Arrange
             // "sans-culture" is an invalid culture name everywhere -- even on Windows 10.
             var context = GetContextForSetCulture(new[] { "sans-culture", "bb-BB", "cc-CC" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentCulture;
 
             // Act
@@ -119,12 +126,13 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureWithInvalidLanguageDoesNothing()
         {
             // Arrange
             // "sans-culture" is an invalid culture name everywhere -- even on Windows 10.
             var context = GetContextForSetCulture(new[] { "sans-culture", "bb-BB", "cc-CC" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
             CultureInfo culture = thread.CurrentUICulture;
 
             // Act
@@ -135,11 +143,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureDetectsUserLanguageCulture()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { "en-GB", "en-US", "ar-eg" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetCulture(thread, context, "auto");
@@ -150,11 +159,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureDetectsUserLanguageCulture()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { "en-GB", "en-US", "ar-eg" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetUICulture(thread, context, "auto");
@@ -165,11 +175,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoCultureUserLanguageWithQParameterCulture()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { "en-GB;q=0.3", "en-US", "ar-eg;q=0.5" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetCulture(thread, context, "auto");
@@ -180,11 +191,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetAutoUICultureDetectsUserLanguageWithQParameterCulture()
         {
             // Arrange
             var context = GetContextForSetCulture(new[] { "en-GB;q=0.3", "en-US", "ar-eg;q=0.5" });
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetUICulture(thread, context, "auto");
@@ -195,33 +207,36 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetCultureWithInvalidCultureThrows()
         {
             // Arrange
             var context = GetContextForSetCulture();
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act and Assert
             Assert.Throws<CultureNotFoundException>(() => CultureUtil.SetCulture(thread, context, "sans-culture"));
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetUICultureWithInvalidCultureThrows()
         {
             // Arrange
             var context = GetContextForSetCulture();
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act and Assert
             Assert.Throws<CultureNotFoundException>(() => CultureUtil.SetUICulture(thread, context, "sans-culture"));
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetCultureWithValidCulture()
         {
             // Arrange
             var context = GetContextForSetCulture();
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetCulture(thread, context, "en-GB");
@@ -232,11 +247,12 @@ namespace System.Web.WebPages.Test
         }
 
         [Fact]
+        [ReplaceCulture(Culture = "es-PR", UICulture = "es-PR")]
         public void SetUICultureWithValidCulture()
         {
             // Arrange
             var context = GetContextForSetCulture();
-            Thread thread = GetThread();
+            Thread thread = Thread.CurrentThread;
 
             // Act
             CultureUtil.SetUICulture(thread, context, "en-GB");
@@ -244,11 +260,6 @@ namespace System.Web.WebPages.Test
             // Assert
             Assert.Equal(CultureInfo.GetCultureInfo("en-GB"), thread.CurrentUICulture);
             Assert.Equal("05/01/1979", new DateTime(1979, 1, 5).ToString("d", thread.CurrentUICulture));
-        }
-
-        private static Thread GetThread()
-        {
-            return new Thread(() => { });
         }
 
         private static HttpContextBase GetContextForSetCulture(IEnumerable<string> userLanguages = null)
