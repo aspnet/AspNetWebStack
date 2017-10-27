eebbf3884628df3502e8055acf8108c123c29d9f
Doug Bunting <dougbu@microsoft.com>
2017-10-27 12:49:15 -0700

    Make subclassing `DefaultBodyModelValidator` more useful - #9 - add `BodyModelValidatorContext` and `IBodyModelValidatorKeyBuilder`

---
+++ src/System.Web.Http/Validation/IBodyModelValidatorKeyBuilder.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace System.Web.Http.Validation
+{
+    /// <summary>
+    /// Abstraction for creating keys used in nested validation scopes. Intended for use in
+    /// <see cref="IBodyModelValidator"/> implementations, especially <see cref="DefaultBodyModelValidator"/>.
+    /// </summary>
+    public interface IBodyModelValidatorKeyBuilder
+    {
+        /// <summary>
+        /// Returns the key for a nested scope within the <paramref name="prefix"/> scope.
+        /// </summary>
+        /// <param name="prefix">Key for the current scope.</param>
+        /// <returns>Key for a nested scope. Usually appends a property name to <paramref name="prefix"/>.</returns>
+        string AppendTo(string prefix);
+    }
+}
--- src/System.Web.Http/Validation/IModelValidatorCache.cs
+++ src/System.Web.Http/Validation/IModelValidatorCache.cs
@@ -1,16 +1,21 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Web.Http.Metadata;
 
 namespace System.Web.Http.Validation
 {
     /// <summary>
-    /// Defines a cache for <see cref="ModelValidator"/>s. This cache is keyed on the type or property that the metadata is associated with.
+    /// Defines a cache for <see cref="ModelValidator"/>s. This cache is keyed on the type or property that the
+    /// metadata is associated with.
     /// </summary>
-    internal interface IModelValidatorCache
+    public interface IModelValidatorCache
     {
+        /// <summary>
+        /// Returns the <see cref="ModelValidator"/>s for the given <paramref name="metadata"/>.
+        /// </summary>
+        /// <param name="metadata">The <see cref="ModelMetadata"/>.</param>
+        /// <returns>An array of <see cref="ModelValidator"/>s for the given <paramref name="metadata"/>.</returns>
         ModelValidator[] GetValidators(ModelMetadata metadata);
     }
 }
