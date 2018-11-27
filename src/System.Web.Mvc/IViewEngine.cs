0f61e94592414c4e9dc57c7b64c16fb96b287ef2
Mansoor Omrani <mansoor.omrani@gmail.com>
2018-11-27 12:25:19 +0330

    Removing unnecessary comments

--- src/System.Web.Mvc/IViewEngine.cs
+++ src/System.Web.Mvc/IViewEngine.cs
@@ -7,10 +7,8 @@ namespace System.Web.Mvc
     {
         ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache);
         ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache);
-        // ------------------- Branch: support_generic_models_in_views (start) -------------------
         ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache, Type[] genericTypes);
         ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache, Type[] genericTypes);
-        // ------------------- Branch: support_generic_models_in_views ( end ) -------------------
         void ReleaseView(ControllerContext controllerContext, IView view);
     }
 }
