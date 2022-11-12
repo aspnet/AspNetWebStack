1c8de17ce15110d00d69a18bd087d40559c3429e
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2022-11-11 17:13:54 -0800

    Build w/ VS 2019; restore w/ `msbuild` - handle both SDK-style and legacy projects in one way   - set `$(RestorePackagesConfig)` to cover both project types - allow newer .NET SDKs to align w/ VS components - make a few `<import/>`s conditional to avoid problems in `Restore` target evaluation - restore global packages.config before building to make StyleCop available - move second copies of packages.config packages into packages/ folder - update FxCop TFM   - .NET 4.0 not supported because current FxCopSdk is built against .NET 4.7.2 - suppress new warnings about old FxCop approach and not using `IFormatProvider`

--- build.cmd
+++ build.cmd
@@ -7,12 +7,11 @@ mkdir bin
 
 :Build
 
-REM Find the most recent 32bit MSBuild.exe on the system. Require v15.0 (installed with VS2017) or later since .NET
-REM Core projects are coming soon.
-REM Use `vswhere` for the search since %ProgramFiles(x86)%\msbuild\15.0\Bin\MSBuild.exe almost never exists.
+REM Find the most recent 32bit MSBuild.exe on the system. Require v16.0 (installed with VS2019) or later.
+REM Use `vswhere` for the search because it can find all VS installations.
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 if not exist %vswhere% (
-  set VsWhere="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+  set vswhere="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
 )
 if not exist %vswhere% (
   REM vswhere.exe not in normal locations; check the Path.
@@ -25,18 +24,28 @@ if not exist %vswhere% (
   goto BuildFail
 )
 
+REM We're fine w/ any .NET SDK newer than 2.1.500 but also need a 2.1.x runtime. Microsoft.Net.Core.Component.SDK.2.1
+REM actually checks for only the runtime these days.
 set InstallDir=
-for /f "usebackq tokens=*" %%i in (`%vswhere% -version ^[15^,16^) -latest -prerelease -products * ^
-    -requires Microsoft.Component.MSBuild -requires Microsoft.Net.Core.Component.SDK.2.1 ^
+for /f "usebackq tokens=*" %%i in (`%vswhere% -version 16 -latest -prerelease -products * ^
+    -requires Microsoft.Component.MSBuild ^
+    -requires Microsoft.NetCore.Component.SDK ^
+    -requires Microsoft.Net.Core.Component.SDK.2.1 ^
     -property installationPath`) do (
-  set InstallDir=%%i
+  set InstallDir="%%i"
 )
-if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
-  set MSBuild="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+
+if exist %InstallDir%\MSBuild\Current\Bin\MSBuild.exe (
+  set MSBuild=%InstallDir%\MSBuild\Current\Bin\MSBuild.exe
 ) else (
-  echo Could not find MSBuild.exe. Please install the VS2017 BuildTools component or a workload that includes it.
+  echo Could not find MSBuild.exe. Please install the VS2019 BuildTools component or a workload that includes it.
   goto BuildFail
 )
+
+REM Configure NuGet operations to work w/in this repo i.e. do not pollute system packages folder.
+REM Note this causes two copies of packages restored using packages.config to land in this folder e.g.
+REM StyleCpy.5.0.0/ and stylecop/5.0.0/.
+set "NUGET_PACKAGES=%CD%\packages"
 
 REM Are we running in a local dev environment (not on CI)?
 if DEFINED CI (set Desktop=false) else if DEFINED TEAMCITY_VERSION (set Desktop=false) else (set Desktop=true)
